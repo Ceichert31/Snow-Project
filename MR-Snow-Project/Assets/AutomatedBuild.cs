8ece33a049bea900aa234700ff32cce943633ad6
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-01-22 09:30:16 -0500

    feat: Added apk builder script

---
+++ MR-Snow-Project/Assets/AutomatedBuild.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using System.IO;
+
+public class AutomatedBuild
+{
+    /// <summary>
+    /// Builds the android APK
+    /// </summary>
+    /// <remarks>
+    /// Called by GitHub action runner
+    /// </remarks>
+    public static void BuildAndroid()
+    {
+        //Add all scenes we want to build
+        string[] scenes = { "Assets/Scenes/SampleScene.unity" };
+
+        string outputPath = "Builds/VRApp.apk"; 
+
+        // Ensure output directory exists
+        Directory.CreateDirectory("Builds");
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = outputPath;
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
