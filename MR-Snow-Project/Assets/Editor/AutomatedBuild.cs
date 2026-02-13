2e03c5928094feaad15c3a0b91e900a6b167b90f
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-02-13 14:32:17 -0500

    fix: Added missing automated build script

---
+++ MR-Snow-Project/Assets/Editor/AutomatedBuild.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutomatedBuild
+{
+    public static void BuildAndroid()
+    {
+        string[] scenes = EditorBuildSettings
+            .scenes.Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+
+        string buildPath = "builds/Android/build.apk";
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = buildPath,
+            target = BuildTarget.Android,
+            options = BuildOptions.Development | BuildOptions.CompressWithLz4, // Faster compression
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log(
+                $"BUILD SUCCEEDED | Size: {report.summary.totalSize} bytes | Time: {report.summary.totalTime}"
+            );
+        }
+        else
+        {
+            Debug.LogError("BUILD FAILED");
+            EditorApplication.Exit(-1);
+        }
+    }
+}
