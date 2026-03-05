a0b8f505bc71c6ee001b656806dd2e1f1450c45b
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-03-05 14:18:22 -0500

    feat: Added permission check for spatial data and integrated snow deformation to XR scene

---
+++ MR-Snow-Project/Assets/Scripts/RequestSpatialPermissions.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Requests spatial data from user, if denied just generate a large plane as the ground plane
+/// </summary>
+public class RequestSpatialPermissions : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent OnPermissionGranted;
+
+    [SerializeField]
+    private UnityEvent OnPermissionDenied;
+    const string spatialPermission = "com.oculus.permission.USE_SCENE";
+
+    private void Awake()
+    {
+        bool hasUserAuthorizedPermission =
+            UnityEngine.Android.Permission.HasUserAuthorizedPermission(spatialPermission);
+
+        if (!hasUserAuthorizedPermission)
+        {
+            var callbacks = new UnityEngine.Android.PermissionCallbacks();
+
+            callbacks.PermissionGranted += OnGranted;
+            callbacks.PermissionDenied += OnDenied;
+
+            UnityEngine.Android.Permission.RequestUserPermission(spatialPermission);
+        }
+    }
+
+    private void OnGranted(string obj)
+    {
+        OnPermissionGranted?.Invoke();
+    }
+
+    private void OnDenied(string obj)
+    {
+        OnPermissionDenied?.Invoke();
+    }
+}
