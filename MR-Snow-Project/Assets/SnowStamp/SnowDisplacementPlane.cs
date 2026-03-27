95a6725debb6fc89c07e17b0eff4dd1061a5ce0b
Joseph Isaacs <joseph.isaacs@mymail.champlain.edu>
2026-03-27 13:32:27 -0400

    Not working anymore, signing off

--- MR-Snow-Project/Assets/SnowStamp/SnowDisplacementPlane.cs
+++ MR-Snow-Project/Assets/SnowStamp/SnowDisplacementPlane.cs
@@ -4,30 +4,26 @@ public class SnowDisplacementPlane : MonoBehaviour
 {
     [SerializeField] private RenderTexture splatMap;
 
-    private Renderer planeRenderer;
-    private MaterialPropertyBlock propertyBlock;
+    private Material planeMaterial;
 
     private static readonly int SplatMapID = Shader.PropertyToID("_SplatMap");
     private static readonly int GroundOriginID = Shader.PropertyToID("_GroundOrigin");
     private static readonly int GroundSizeID = Shader.PropertyToID("_GroundSize");
 
-    private void Awake()
+    private void Start()
     {
-        planeRenderer = GetComponent<Renderer>();
-        propertyBlock = new MaterialPropertyBlock();
+        planeMaterial = GetComponent<Renderer>().material;
     }
 
     private void Update()
     {
-        if (splatMap == null)
+        if (splatMap == null || planeMaterial == null)
             return;
 
         Vector3 scale = transform.lossyScale;
 
-        propertyBlock.SetTexture(SplatMapID, splatMap);
-        propertyBlock.SetVector(GroundOriginID, new Vector2(transform.position.x, transform.position.z));
-        propertyBlock.SetVector(GroundSizeID, new Vector2(scale.x, scale.z));
-
-        planeRenderer.SetPropertyBlock(propertyBlock);
+        planeMaterial.SetTexture(SplatMapID, splatMap);
+        planeMaterial.SetVector(GroundOriginID, new Vector2(transform.position.x, transform.position.z));
+        planeMaterial.SetVector(GroundSizeID, new Vector2(scale.x, scale.z));
     }
 }
