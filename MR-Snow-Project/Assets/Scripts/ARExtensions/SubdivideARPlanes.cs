16f3e87479d0af7d1c39823f78090a582db29006
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-04-02 11:52:55 -0400

    refactor: First attempt at implementing subdivision in mesh generation script

--- MR-Snow-Project/Assets/Scripts/ARExtensions/SubdivideARPlanes.cs
+++ MR-Snow-Project/Assets/Scripts/ARExtensions/SubdivideARPlanes.cs
@@ -49,7 +49,7 @@ namespace ARExtensions
         }
 
         /// <summary>
-        /// Creates a subdivied mesh and returns in
+        /// Creates a subdivided mesh and returns in
         /// </summary>
         /// <returns></returns>
         private Mesh CreateSubdividedMesh(int sizeX, int sizeY, int subdivisions)
@@ -58,24 +58,31 @@ namespace ARExtensions
 
             mesh.name = $"Subdivided Mesh ({subdivisionCount})";
 
+            int vertsPerSide = subdivisionCount + 1;
+
             //Initialize an array big enough to hold all vertices data
-            Vector3[] verts = new Vector3[((sizeX + 1) * (sizeY + 1))];
+            Vector3[] verts = new Vector3[vertsPerSide * vertsPerSide];
             Vector2[] uvs = new Vector2[verts.Length];
 
+            //Need to swap from generating 4 verts based on sizex sizey to generating subdivisions
+
+
             //Create new vertices
-            for (int x = 0, i = 0; x < sizeX; x++)
+            for (int x = 0, i = 0; x < vertsPerSide; x++)
             {
                 for (int y = 0; y < sizeY; y++, i++)
                 {
-                    verts[i] = new Vector3(x, 0, y);
-                    uvs[i] = new Vector2((float)x / sizeX, (float)y / sizeY);
+                    float u = (float)x / subdivisions;
+                    float v = (float)y / subdivisions;
+
+                    verts[i] = new Vector3((u - 0.5f) * sizeX, 0, (v - 0.5f) * sizeY);
+                    uvs[i] = new Vector2(u, v);
                 }
             }
 
+            int[] tris = new int[subdivisions * subdivisions * 6];
 
-            int[] tris = new int[sizeX * 6];
-
-            //Calculate triangles
+            /*//Calculate triangles
             for (int i = 0, j = 0, x = 0; x < sizeX; x++, i += 6, j++)
             {
                 for (int y = 0; y < sizeY; y++, i += 6, j++)
@@ -84,6 +91,28 @@ namespace ARExtensions
                     tris[j + 3] = tris[i + 2] = j + 1;
                     tris[i + 4] = tris[i + 1] = j + sizeX + 1;
                     tris[i + 5] = j + sizeX + 2;
+                }
+            }*/
+
+            for (int x = 0, t = 0; x < subdivisions; x++)
+            {
+                for (int y = 0; y < subdivisions; y++)
+                {
+                    //Calculate corners of quad
+                    int bottomLeft = x * vertsPerSide + y;
+                    int bottomRight = bottomLeft + 1;
+                    int topLeft = bottomLeft + vertsPerSide;
+                    int topRight = topLeft + 1;
+
+                    //First triangle
+                    tris[t++] = bottomLeft;
+                    tris[t++] = topLeft;
+                    tris[t++] = bottomRight;
+
+                    //Second triangle
+                    tris[t++] = bottomRight;
+                    tris[t++] = topLeft;
+                    tris[t++] = topRight;
                 }
             }
 
