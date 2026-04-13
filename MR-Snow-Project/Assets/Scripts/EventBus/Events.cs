457a518fc671d22ac54b7c400de3e2b22ad471b0
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-04-12 20:30:04 -0400

    feat: Setup tesselation testing scene

--- MR-Snow-Project/Assets/Scripts/EventBus/Events.cs
+++ MR-Snow-Project/Assets/Scripts/EventBus/Events.cs
@@ -9,4 +9,5 @@ public class SettingsEvent : IEvent
 {
     public int snowSpawnRate = 100;
     public bool isSnowEnabled = true;
+    public int windRate = 1;
 }
--- MR-Snow-Project/Assets/Scripts/UI/UpdateSettingsValues.cs
+++ MR-Snow-Project/Assets/Scripts/UI/UpdateSettingsValues.cs
@@ -1,26 +1,30 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class UpdateSettingsValues : MonoBehaviour
 {
-    [Header("UI References")]
-    [SerializeField]
+    [Header("UI References")] [SerializeField]
     private Slider spawnRateSlider;
 
-    [SerializeField]
-    private Toggle snowEnabledToggle;
+    [SerializeField] private Slider windRateSlider;
+
+    [SerializeField] private Toggle snowEnabledToggle;
 
     private SettingsEvent settingsEvent = new();
 
     private void OnEnable()
     {
         spawnRateSlider.onValueChanged.AddListener(UpdateSpawnRate);
+        windRateSlider.onValueChanged.AddListener(UpdateWindRate);
         snowEnabledToggle.onValueChanged.AddListener(UpdateSnowToggle);
     }
 
     private void OnDisable()
     {
         spawnRateSlider.onValueChanged.RemoveListener(UpdateSpawnRate);
+        windRateSlider.onValueChanged.RemoveListener(UpdateWindRate);
         snowEnabledToggle.onValueChanged.RemoveListener(UpdateSnowToggle);
     }
 
@@ -33,6 +37,12 @@ public class UpdateSettingsValues : MonoBehaviour
     private void UpdateSnowToggle(bool isEnabled)
     {
         settingsEvent.isSnowEnabled = isEnabled;
+        EventBus<SettingsEvent>.Raise(settingsEvent);
+    }
+
+    private void UpdateWindRate(float windRate)
+    {
+        settingsEvent.windRate = (int)windRate;
         EventBus<SettingsEvent>.Raise(settingsEvent);
     }
 }
